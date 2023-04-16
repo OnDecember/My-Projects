5c481ace7352995af833acb8e047bedcffe31530
OnDecember <so.unique.december@gmail.com>
2023-04-16 21:24:41 +0300

    add new methods

--- MyCollections/MyArrayList/src/Main.java
+++ MyCollections/MyArrayList/src/Main.java
@@ -1,7 +1,73 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
 
+        List<Integer> list1 = new ArrayList<>();
+
+        System.out.println("Create List");
+        System.out.println("List : " + list);
+        System.out.println("Size : " + list.size() + "\n");
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        System.out.println("Add new elements");
+        System.out.println("List : " + list);
+        System.out.println("Size : " + list.size() + "\n");
+
+        list.add(1, 15);
+        System.out.println("Add new element to the list with index 1");
+        System.out.println("List : " + list);
+        System.out.println("Size : " + list.size() + "\n");
+
+        System.out.println("Get element with index 1");
+        System.out.println("List : " + list);
+        System.out.println("Element : " + list.get(1));
+        System.out.println("Size : " + list.size() + "\n");
+
+        System.out.println("Search element with value 3");
+        System.out.println("List : " + list);
+        System.out.println("Index : " + list.search(3));
+        System.out.println("Size : " + list.size() + "\n");
+
+        list.set(0, 20);
+        System.out.println("Replace element with index 0");
+        System.out.println("List : " + list);
+        System.out.println("Size : " + list.size() + "\n");
+
+        list.remove(3);
+        System.out.println("Remove element with index 3");
+        System.out.println("List : " + list);
+        System.out.println("Size : " + list.size() + "\n");
+
+        boolean isRemove = list.remove(Integer.valueOf(3));
+        System.out.println("Remove element with value 3");
+        System.out.println("List : " + list);
+        System.out.println("Is Remove : " + isRemove);
+        System.out.println("Size : " + list.size() + "\n");
+
+        boolean isEmpty = list.isEmpty();
+        System.out.println("Check list for emptiness");
+        System.out.println("List : " + list);
+        System.out.println("Is Empty : " + isEmpty);
+        System.out.println("Size : " + list.size() + "\n");
+
+        list.addAll(new Integer[]{10, 20, 30});
+        System.out.println("Add new elements to the list");
+        System.out.println("List : " + list);
+        System.out.println("Size : " + list.size() + "\n");
+
+        list.addAll(0, new Integer[]{1, 2, 3});
+        System.out.println("Add new elements to the list with index 0");
+        System.out.println("List : " + list);
+        System.out.println("Size : " + list.size() + "\n");
+
+        list.clear();
+        System.out.println("Clear list");
+        System.out.println("List : " + list);
+        System.out.println("Size : " + list.size());
     }
 }
--- MyCollections/MyArrayList/src/MyArrayList.java
+++ MyCollections/MyArrayList/src/MyArrayList.java
@@ -3,8 +3,15 @@ import java.util.Arrays;
 public class MyArrayList<E> {
 
     private int size;
-    private final int DEFAULT_SIZE = 10;
-    private Object[] list = new Object[DEFAULT_SIZE];
+    private int defaultSize = 10;
+    private Object[] list = new Object[defaultSize];
+
+    public MyArrayList() {
+    }
+
+    public MyArrayList(int size) {
+        this.defaultSize = size;
+    }
 
     public void add(E element) {
         if (size == list.length) {
@@ -14,30 +21,108 @@ public class MyArrayList<E> {
     }
 
     public void add(int index, E element) {
-        if (index < 0 || index > size) {
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException("Invalid index : " + index + ", expected index between 0 and " + size);
         }
         if (size == list.length) {
             grow(size);
         }
-        list = insert(list, index);
+        list = insert(list, index, 1);
         list[index] = element;
         size++;
+    }
+
+    public void addAll(E... elements) {
+        for (E element : elements) {
+            add(element);
+        }
+    }
+
+    public void addAll(int index, E... elements) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Invalid index : " + index + ", expected index between 0 and " + size);
+        }
+        if (size == list.length) {
+            grow(size);
+        }
+        int length = elements.length;
+        list = insert(list, index, length);
+        for (int i = 0; i < length; i++) {
+            list[index + i] = elements[i];
+            size++;
+        }
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Invalid index : " + index + ", expected index between 0 and " + size);
+        }
+        return (E) list[index];
+    }
+
+    public int search(E element) {
+        for (int i = 0; i < size; i++) {
+            if (list[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void set(int index, E element) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Invalid index : " + index + ", expected index between 0 and " + size);
+        }
+        list[index] = element;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Invalid index : " + index + ", expected index between 0 and " + size);
+        }
+        list = delete(list, index);
+        size--;
+    }
+
+    public boolean remove(E element) {
+        int index = search(element);
+        if (index != -1) {
+            list = delete(list, index);
+            size--;
+            return true;
+        }
+        return false;
     }
 
     private void grow(int size) {
         list = Arrays.copyOf(list, (int) (size * 1.5 + 1));
     }
 
-    private Object[] insert(Object[] array, int index) {
-        Object[] copy = new Object[array.length + 1];
+    private Object[] insert(Object[] array, int index, int countElements) {
+        Object[] copy = new Object[array.length + countElements];
         System.arraycopy(array, 0, copy, 0, index);
-        System.arraycopy(array, index, copy, index + 1, array.length - index);
+        System.arraycopy(array, index, copy, index + countElements, array.length - index);
+        return copy;
+    }
+
+    private Object[] delete(Object[] array, int index) {
+        Object[] copy = new Object[array.length - 1];
+        System.arraycopy(array, 0, copy, 0, index);
+        System.arraycopy(array, index + 1, copy, index, copy.length - index);
         return copy;
     }
 
     public int size() {
         return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        list = new Object[defaultSize];
+        size = 0;
     }
 
     @Override
